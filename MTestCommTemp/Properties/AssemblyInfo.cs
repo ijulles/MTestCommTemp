0bbb6de9b319cbcf2ab37374e78399d493025147
Julles <Julles@qq.com>
2021-06-30 19:02:23 +0800

    更改生成应用程序名称。

--- MTestCommTemp/Properties/AssemblyInfo.cs
+++ MTestCommTemp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("WAGOTemplate")]
+[assembly: AssemblyTitle("MTestCommTemp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WAGOTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyProduct("MTestCommTemp")]
+[assembly: AssemblyCopyright("ITI Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
